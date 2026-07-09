93fc68c04231301c3237ae9cb5b578ca9530c2af
tapheret2 <tapheret2@users.noreply.github.com>
2026-07-09 14:02:27 +0700

    chore: bump package __version__ to 0.1.1

--- src/brier_lab/__init__.py
+++ src/brier_lab/__init__.py
@@ -1,4 +1,4 @@
 from brier_lab.metrics import brier_score, log_loss, mean_brier, reliability_bins
 
 __all__ = ["brier_score", "log_loss", "mean_brier", "reliability_bins"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
